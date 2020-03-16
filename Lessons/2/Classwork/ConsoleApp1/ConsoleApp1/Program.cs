052d13783d51d64cba8f0dd1cd3efe6373d45413
juliiasheikina <your@email.com>
2020-03-16 22:09:46 +0300

    Lesson#2 Last version

---
+++ Lessons/2/Classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 12;
+            short b = 2;
+            float c = 20.01f;
+            decimal d = 10.00M;
+
+            Console.WriteLine($"int is: {a}");
+            Console.WriteLine($"short is: {b}");
+            Console.WriteLine($"float is: {c}");
+            Console.WriteLine($"decimal is: {d}");*/
+            string line = Console.ReadLine();
+            decimal number = decimal.Parse(line);
+            Console.WriteLine(number);
+            
+        }
+    }
+}
