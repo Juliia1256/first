a1ba1a4f62b0db3abaedb82ee0e5cb8a1c5f3177
Juliia Sheikina <sheik1256@bk.ru>
2020-03-21 17:54:49 +0300

    Home work 3

---
+++ Lessons/3/HomeWork/HomeWork3JSH/HomeWork3JSH/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace HomeWork3JSH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //declaring an array requesting names
+
+                Console.WriteLine("Please, enter Your name:");
+                string[] usersname = new string[3];
+                
+                    for (var i = 0; i < 3; i++)
+                    {
+                        usersname[i] = Console.ReadLine();
+                    }
+                //declaring an array requesting age
+
+                Console.WriteLine("Please, enter Your age:");
+                int[] usersage = new int[3];
+               
+                for ( int h = 0; h < 3; h++)
+                {
+                        usersage[h] = Convert.ToInt32(Console.ReadLine()); 
+                }
+                //output array values, users age after 4 years
+                int b = 4;
+                for (var a = 0; a < 3; a++)
+                {
+                    Console.WriteLine("Name:" + usersname[a] + ", age in 4 years: " + Convert.ToInt32(usersage[a] + b));
+                }
+
+            }
+        }
+    }
+}
