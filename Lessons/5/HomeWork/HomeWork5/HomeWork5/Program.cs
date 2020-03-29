6f593975420eeb7d8c5260a8fba85175f746ee96
Juliia Sheikina <sheik1256@bk.ru>
2020-03-29 20:30:17 +0300

    Home Work #5

--- Lessons/5/HomeWork/HomeWork5/HomeWork5/Program.cs
+++ Lessons/5/HomeWork/HomeWork5/HomeWork5/Program.cs
@@ -11,32 +11,73 @@ namespace HomeWork5
             rectangle = 3
         }
         static void Main(string[] args)
-        {
-            Console.Write("Выберите фигуру из списка: 1 - Круг, 2 - Равносторонний треугольник, 3 - Прямоугольник: ");
-            var userAnswer = int.Parse(Console.ReadLine());
-            switch userAnswer
-                case figures.circle:
-                var diametr = 
-                
-
-            /*static int ReadInt();
+        { // method for handling user input
+            static int ReadInt()
             {
                 for (; ;)
                 {
                     try
                     {
-                        Console.Write(); // add parameters
+                        Console.WriteLine(); 
                         return int.Parse(Console.ReadLine());
                     }
                     catch (FormatException exception)
                     {
-                        Console.WriteLine("Entered wrong value");
+                        Console.WriteLine("Введены не корректные параметры, попробуйте снова");
                     }
                 }
             }
-            */
-
-
+            //the loop allows you to continue the program after handling the exception
+            for (;;)
+                try
+                {
+                    Console.Write("Выберите фигуру из списка: 1 - Круг, 2 - Равносторонний треугольник, 3 - Прямоугольник: ");
+                    var answer = Enum.Parse(typeof(figures), Console.ReadLine());
+                    try
+                    {
+                        switch (answer)
+                        {
+                            case figures.circle:
+                                Console.Write("Введите диаметр круга: ");
+                                var diametrC = ReadInt();
+                                Console.WriteLine($"Площадь круга равна {(Math.PI * Math.Pow(diametrC, 2)) / 4}");
+                                Console.WriteLine($"Периметр круга равен {diametrC * Math.PI}");
+                                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                                Console.ReadLine();
+                                break;
+                            case figures.triangle:
+                                Console.Write("Введите длину основания треугольника: ");
+                                var osnovT = ReadInt();
+                                var hightT = 0.5 * Math.Sqrt(4 * Math.Pow(osnovT, 2) - Math.Pow(osnovT, 2));
+                                Console.WriteLine($"Площадь треугольника равна {(osnovT * hightT) / 2}");
+                                Console.WriteLine($"Пеример треугольника равен {osnovT + (2 * osnovT)}");
+                                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                                Console.ReadLine();
+                                break;
+                            case figures.rectangle:
+                                Console.Write("Введите длину прямоугольника: ");
+                                var lengthR = ReadInt();
+                                Console.Write("Введите ширину прямоугольника: ");
+                                var widthR = ReadInt();
+                                Console.WriteLine($"Площадь прямоугольника равна {lengthR * widthR}");
+                                Console.WriteLine($"Пеример прямоугольника равен {2 * lengthR + 2 * widthR}");
+                                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                                Console.ReadLine();
+                                break;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Выбрано значение вне диапозона 1-3, символ или буква. Нажмите любую клавишу для повторного ввода");
+                        Console.ReadLine();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Выбрано значение вне диапозона 1-3, символ или буква. Нажмите любую клавишу для повторного ввода");
+                    Console.ReadLine();
+                }
         }
     }
 }
+
