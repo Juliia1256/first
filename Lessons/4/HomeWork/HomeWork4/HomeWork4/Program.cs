de13d1fad19f9f27344d09a6bbb430b6718a5454
Juliia Sheikina <sheik1256@bk.ru>
2020-03-29 21:20:10 +0300

    Home work #3 with corrected comments

--- Lessons/4/HomeWork/HomeWork4/HomeWork4/Program.cs
+++ Lessons/4/HomeWork/HomeWork4/HomeWork4/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Dynamic;
 
 namespace HomeWork4
 {
     enum Containers :byte
     {   
         conttwenty = 0b00010100,
-        contfive = 0b00000101,
-        contone = 0b00000001
+        contfive =   0b00000101,
+        contone =    0b00000001
     }
     class Program
     {
@@ -17,15 +16,33 @@ namespace HomeWork4
 
             var desiredvolume = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Вам потребуются следующие контейнеры:");
+            
 
             int fulltwenty = (int)desiredvolume / (int)Containers.conttwenty;
-
-            if (fulltwenty > 0)
+            var flagfulltwenty = fulltwenty | (int)Containers.conttwenty;
+            if (fulltwenty > 0) //в этом случае работает, и при пустом else контейнеры не выводятся
+            //if ((flagfulltwenty & (int)Containers.conttwenty) == (int)Containers.conttwenty)
+                /* например, мы ввели число 21, тогда, при включении флага, переменная = 20 (стр.23):
+                   |
+                 0   0  0
+                 1   1  1
+                 0   0  0
+                 1   1  1
+                 0   0  0
+                 1   0  0
+                 при проверке в части if(стр.24) мы объединяем переменную с флагом и контейнер 20 и далее сравниваем результат с контейнером 20, но он уже равен 20! Зачем?:
+                    &       ==
+                 0   0  0    0
+                 1   1  1    1
+                 0   0  0    0
+                 1   1  1    1
+                 0   0  0    0
+                 0   0  0    0 */
             {
                 Console.WriteLine($" 20 л: {fulltwenty} шт.");
- 
             }
-            else 
+            //else if ((flagfulltwenty & (int)Containers.conttwenty) == 0) 
+            else
             {
             }
             double balancetwenty = (double)desiredvolume % (double)Containers.conttwenty;
