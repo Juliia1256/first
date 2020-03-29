f34a754252daab41515b143d66a4de87398c543c
Juliia Sheikina <sheik1256@bk.ru>
2020-03-29 21:03:25 +0300

    Home work #3 with corrected comments

--- Lessons/3/HomeWork/HomeWork3JSH/HomeWork3JSH/Program.cs
+++ Lessons/3/HomeWork/HomeWork3JSH/HomeWork3JSH/Program.cs
@@ -9,31 +9,36 @@ namespace HomeWork3JSH
         {
             {
                 //declaring an array requesting names
-                int a = 3;
+                int sizearray = 3;
                 Console.WriteLine("Please, enter Your name:");
-                string[] usersname = new string[a];
-                
+                string[] usersname = new string[sizearray];
+                try
+                {
                     for (var i = 0; i < 3; i++)
                     {
                         usersname[i] = Console.ReadLine();
                     }
-                //declaring an array requesting age
+                    //declaring an array requesting age
 
-                Console.WriteLine("Please, enter Your age:");
-                int[] usersage = new int[a];
-               
-                for ( int i = 0; i < 3; i++)
-                {
-                        usersage[i] = Convert.ToInt32(Console.ReadLine()); 
+                    Console.WriteLine("Please, enter Your age:");
+                    int[] usersage = new int[sizearray];
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        usersage[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    //output array values, users age after 4 years
+                    int aging = 4;
+                    for (var i = 0; i < 3; i++)
+                    {
+                        Console.WriteLine("Name:" + usersname[i] + ", age in 4 years: " + (usersage[i] + aging));
+                    }
                 }
-
-                //output array values, users age after 4 years
-                int b = 4;
-                for (var i = 0; i < 3; i++)
+                catch
                 {
-                    Console.WriteLine("Name:" + usersname[i] + ", age in 4 years: " + (usersage[i] + b));
+                    Console.WriteLine("Incorrect data entered");
                 }
-
             }
         }
     }
