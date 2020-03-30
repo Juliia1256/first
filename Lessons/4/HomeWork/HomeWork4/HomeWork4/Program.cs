024bfcfad7bdfaa6ef6d821a40dfda1bf7e2951c
Juliia Sheikina <sheik1256@bk.ru>
2020-03-30 15:22:24 +0300

    HW #4 corrected

--- Lessons/4/HomeWork/HomeWork4/HomeWork4/Program.cs
+++ Lessons/4/HomeWork/HomeWork4/HomeWork4/Program.cs
@@ -8,67 +8,57 @@ namespace HomeWork4
         contfive =   0b00000101,
         contone =    0b00000001
     }
+    enum ContType :byte
+    {
+        typetwenty = 0b00000100,
+        typefive =   0b00000010,
+        typeone =    0b00000001
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Какой объем сока (в литрах) требуется упаковать?");
-
             var desiredvolume = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Вам потребуются следующие контейнеры:");
-            
 
             int fulltwenty = (int)desiredvolume / (int)Containers.conttwenty;
-            var flagfulltwenty = fulltwenty | (int)Containers.conttwenty;
-            if (fulltwenty > 0) //в этом случае работает, и при пустом else контейнеры не выводятся
-            //if ((flagfulltwenty & (int)Containers.conttwenty) == (int)Containers.conttwenty)
-                /* например, мы ввели число 21, тогда, при включении флага, переменная = 20 (стр.23):
-                   |
-                 0   0  0
-                 1   1  1
-                 0   0  0
-                 1   1  1
-                 0   0  0
-                 1   0  0
-                 при проверке в части if(стр.24) мы объединяем переменную с флагом и контейнер 20 и далее сравниваем результат с контейнером 20, но он уже равен 20! Зачем?:
-                    &       ==
-                 0   0  0    0
-                 1   1  1    1
-                 0   0  0    0
-                 1   1  1    1
-                 0   0  0    0
-                 0   0  0    0 */
+            var flagfulltwenty = fulltwenty | (byte)ContType.typetwenty; //add flag
+            flagfulltwenty = flagfulltwenty & (byte)ContType.typetwenty; //check with container
+            double balancetwenty = (double)desiredvolume % (double)Containers.conttwenty; //remainder of division for calculation
+            
+            int fullfive = (int)balancetwenty / (int)Containers.contfive;
+            var flagfullfive = fullfive | (byte)ContType.typefive; //add flag
+            flagfullfive = flagfullfive & (byte)ContType.typefive; //check with container
+            double balancefive = (double)balancetwenty % (double)Containers.contfive; //remainder of division for calculation
+
+            double fullone = Math.Ceiling((double)balancefive / (double)Containers.contone); //rounding operation
+            var flagfullone = (int)fullone | (byte)ContType.typeone; //add flag
+            flagfullone = flagfullone & (byte)ContType.typeone; //check with container
+
+
+            if (flagfulltwenty == (byte)ContType.typetwenty) //check availability
             {
                 Console.WriteLine($" 20 л: {fulltwenty} шт.");
             }
-            //else if ((flagfulltwenty & (int)Containers.conttwenty) == 0) 
-            else
+            else if (flagfulltwenty != (byte)ContType.typetwenty)
             {
             }
-            double balancetwenty = (double)desiredvolume % (double)Containers.conttwenty;
-            
-
-            int fullfive = (int)balancetwenty / (int)Containers.contfive;
-            if (fullfive > 0)
+            if (flagfullfive == (byte)ContType.typefive) //check availability
             {
-                Console.WriteLine($" 5 л: {fullfive} шт.");
-
+                    Console.WriteLine($" 5 л: {fullfive} шт.");
             }
-            else
+            else if (flagfullfive != (byte)ContType.typefive)
             {
             }
-            double balancefive = (double)balancetwenty % (double)Containers.contfive;
-           
-            double fullone = Math.Ceiling((double)balancefive / (double)Containers.contone);
-            if (fullone > 0)
+            if (flagfullone == (byte)ContType.typeone) //check availability
             {
-                Console.WriteLine($" 1 л: {fullone} шт.");
+            Console.WriteLine($" 1 л: {fullone} шт.");
             }
             else
             {
             }
-            Console.ReadLine();
+            Console.ReadLine();   
         }
-
     }
 }
