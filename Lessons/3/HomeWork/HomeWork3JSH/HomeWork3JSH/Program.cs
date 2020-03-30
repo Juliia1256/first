7c0839426e36a379154a6230158950b1eb6967a3
Juliia Sheikina <sheik1256@bk.ru>
2020-03-30 15:15:40 +0300

    HW #3 corrected

--- Lessons/3/HomeWork/HomeWork3JSH/HomeWork3JSH/Program.cs
+++ Lessons/3/HomeWork/HomeWork3JSH/HomeWork3JSH/Program.cs
@@ -8,37 +8,34 @@ namespace HomeWork3JSH
         static void Main(string[] args)
         {
             {
+                
+                Console.WriteLine("Please, enter the length of array:");
+                int count = int.Parse(Console.ReadLine());
+
                 //declaring an array requesting names
-                int sizearray = 3;
                 Console.WriteLine("Please, enter Your name:");
-                string[] usersname = new string[sizearray];
-                try
-                {
-                    for (var i = 0; i < 3; i++)
+                string[] usersname = new string [count];
+
+                    for (var i = 0; i < usersname.Length; i++)
                     {
                         usersname[i] = Console.ReadLine();
                     }
                     //declaring an array requesting age
 
                     Console.WriteLine("Please, enter Your age:");
-                    int[] usersage = new int[sizearray];
+                    int[] usersage = new int[count];
 
-                    for (int i = 0; i < 3; i++)
+                    for (int i = 0; i < usersage.Length; i++)
                     {
                         usersage[i] = Convert.ToInt32(Console.ReadLine());
                     }
 
                     //output array values, users age after 4 years
                     int aging = 4;
-                    for (var i = 0; i < 3; i++)
+                    for (var i = 0; i < usersname.Length; i++) //output equals typed names
                     {
                         Console.WriteLine("Name:" + usersname[i] + ", age in 4 years: " + (usersage[i] + aging));
                     }
-                }
-                catch
-                {
-                    Console.WriteLine("Incorrect data entered");
-                }
             }
         }
     }
