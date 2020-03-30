5ecf721223adb47a7673a7baeb4742a1602fba85
Juliia Sheikina <sheik1256@bk.ru>
2020-03-30 19:28:18 +0300

    CW

---
+++ Lessons/6/ClassWork/Classwork6/Classwork6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classwork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string: ");
+            do
+            {
+                Console.ReadLine();
+                continue;
+            }
+            while (Console.ReadLine() != "exit");
+
+        }
+    }
+}
