b424e9c010964d6f3131b9fe0a5803e4e128c488
Juliia Sheikina <sheik1256@bk.ru>
2020-04-03 17:42:36 +0300

    HW-1, corrected

--- Lessons/6/HomeWork/HW1/HW6-1/HW6-1/Program.cs
+++ Lessons/6/HomeWork/HW1/HW6-1/HW6-1/Program.cs
@@ -4,66 +4,55 @@ namespace HW6_1
 {
     class Program
     {
-        static int CheckInt()
+        static int CheckIntFormat()
         {
+            Console.WriteLine($"Введите натуральное число больше 0 и меньше 2 000 000 000: ");
             int upperLimit = 2000000000; // if you want change a condition
-            for (; ; )
+            while (true)
             {
                 try
-                { 
-                    Console.WriteLine();
+                {
+
                     var check = int.Parse(Console.ReadLine());
                     if (upperLimit >check && check > 0)
                     {
+                        Console.Write($"Введено число {check}. ");
+               
                         return check;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Введенные параметры вне доступного диапазона");
+                        
                     }
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Введены не корректные параметры, попробуйте снова");
+                    Console.Write("Введены не корректные параметры, попробуйте снова");
                 }
                 catch (System.OverflowException)
                 {
-                    Console.WriteLine("Введено число большего значения, чем указано в инструкции");
-                }
-                catch 
-                {
-                    Console.WriteLine("Упс! Что-то пошло не так...");
+                    Console.Write("Введено число большего значения, чем указано в инструкции");
                 }
             }
-
         }
-        static int Counter(string content)
+        static int Counter(string answer)
         {
-            int characters = content.Length;
-            Console.WriteLine($"Количество символов в полученном числе {characters}");
             int evenElements = 0;
-            for (int i=1; i<characters; i++)
+            for (int i=1; i<answer.Length; i++)
             {
-                string b = content.Substring(i, 1);
-                var c = int.Parse(b);
-                int condition = c % 2;
+                string b = answer.Substring(i, 1);
+                int condition = int.Parse(b) % 2;
                 if (condition == 0)
                 {
                     evenElements = evenElements + 1;
                 }
             }
+            Console.WriteLine($@"Количество символов в полученном числе {answer.Length}.
+            Количество четных элементов {evenElements}.");
             return evenElements;
         }
         static void Main(string[] args)
         {
-
-            Console.Write($"Введите натуральное число больше 0 и меньше 2 000 000 000:");
-            var answer = CheckInt();
-            Console.WriteLine($"Введено число {answer}");
-            var content = Convert.ToString(answer);
-       
-            Console.WriteLine($"Количество четных элементов {Counter(content)}");
-            Console.ReadLine();
+            var answer = CheckIntFormat();
+            answer = Counter(Convert.ToString(answer));
+            
         }  
     }
 }
