4cb8f9ae5b8fbf8b6bf09f49c440f8801dd4cabe
Juliia Sheikina <sheik1256@bk.ru>
2020-04-04 16:58:10 +0300

    HW7

---
+++ Lessons/7/HomeWork/HW_1/HW7_1/HW7_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HW7_1
+{
+    class Program
+    {
+        static string CheckStringFormatAndLetterCounter()
+        {
+            Console.WriteLine($"Введите предложение минимум из 2х слов: ");
+            while (true)
+            {
+                try
+                {
+                    var forCheck = Console.ReadLine();
+                    var treatment = forCheck.Split(' '); //break the string into an array of words
+                    var count = 0;
+                    var lettercount = 0;
+                    foreach (string str in treatment)     
+                    {
+                        for (var i = 0; i < treatment.Length; i++)     //count the number of words, if less than two, repeat
+                            lettercount += 1;
+                            if (lettercount < 2)
+                            {
+                                Console.WriteLine($"Введено меньше двух слов, повторите попытку:");
+                                break;
+                            }
+                    }
+                    if (lettercount >= 2)
+                    {
+                        foreach (string letter in treatment)
+                        {
+                            if (letter.StartsWith("a", StringComparison.InvariantCultureIgnoreCase))   //if more than two words, consider starting with the letter "a"
+                            {
+                                count += 1;
+                            }
+                        }
+                        string positivanswer = $"Количество слов, на чинающихся с буквы 'А' равно {count}";
+                        string negativeanswer = $"В введенном предложении нет слов, начинающихся с буквы 'А'.";
+                        var msg = (count > 0) ? positivanswer : negativeanswer;                 //choose the answer depending on the result of the count
+                        return msg;
+                    }
+                }       
+                catch (FormatException)
+                {
+                    Console.Write("Введены не корректные параметры, попробуйте снова");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           Console.WriteLine(CheckStringFormatAndLetterCounter());
+        }
+    }
+}
---
+++ Lessons/7/HomeWork/HW_2/HW7_2/HW7_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW7_2
+{
+    class Program
+    {
+        static string CheckStringFornatOrEmptySpase()
+        {
+            string checkstring= null;
+            Console.WriteLine($"Введите предложение: ");
+            do
+            {                     
+                    checkstring = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(checkstring))
+                    {
+                        Console.WriteLine("Попробуйте еще раз");
+                    }
+            }
+            while (string.IsNullOrWhiteSpace(checkstring));
+            return checkstring.ToLower();
+        }
+        static string StringInReverseArray()
+        {
+            string toreversearray = CheckStringFornatOrEmptySpase();
+            char [] letterarray = toreversearray.ToCharArray();
+
+            Array.Reverse(letterarray);
+            letterarray.ToString();
+            string solution = string.Join("", letterarray);
+            Console.WriteLine(solution);
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+            return solution;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            StringInReverseArray();
+
+        }
+    }
+}
