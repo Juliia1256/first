e4369dfcf5c026e3ebee360bd11aab1af923ab8b
Juliia Sheikina <sheik1256@bk.ru>
2020-04-05 15:32:32 +0300

    HW7_1, corrected

--- Lessons/7/HomeWork/HW_1/HW7_1/HW7_1/Program.cs
+++ Lessons/7/HomeWork/HW_1/HW7_1/HW7_1/Program.cs
@@ -4,51 +4,48 @@ namespace HW7_1
 {
     class Program
     {
-        static string CheckStringFormatAndLetterCounter()
+        static string []  CheckString()
         {
             Console.WriteLine($"Введите предложение минимум из 2х слов: ");
-            while (true)
+            string [] treatment = null;
+            do
             {
-                try
+
+                var forCheck = Console.ReadLine();
+                treatment = forCheck.Split(' '); //break the string into an array of words
+                if (treatment.Length < 2)
                 {
-                    var forCheck = Console.ReadLine();
-                    var treatment = forCheck.Split(' '); //break the string into an array of words
-                    var count = 0;
-                    var lettercount = 0;
-                    foreach (string str in treatment)     
-                    {
-                        for (var i = 0; i < treatment.Length; i++)     //count the number of words, if less than two, repeat
-                            lettercount += 1;
-                            if (lettercount < 2)
-                            {
-                                Console.WriteLine($"Введено меньше двух слов, повторите попытку:");
-                                break;
-                            }
-                    }
-                    if (lettercount >= 2)
-                    {
-                        foreach (string letter in treatment)
-                        {
-                            if (letter.StartsWith("a", StringComparison.InvariantCultureIgnoreCase))   //if more than two words, consider starting with the letter "a"
-                            {
-                                count += 1;
-                            }
-                        }
-                        string positivanswer = $"Количество слов, на чинающихся с буквы 'А' равно {count}";
-                        string negativeanswer = $"В введенном предложении нет слов, начинающихся с буквы 'А'.";
-                        var msg = (count > 0) ? positivanswer : negativeanswer;                 //choose the answer depending on the result of the count
-                        return msg;
-                    }
-                }       
-                catch (FormatException)
+                    Console.WriteLine($"Введено меньше двух слов, повторите попытку:");
+                }
+        
+            }
+            while (treatment.Length < 2);
+
+            return treatment;
+            
+        }
+
+        static string СalculationString()
+        {
+            var processedarray = CheckString();
+            var count = 0;
+            foreach (string letter in processedarray)
+            {
+                if (letter.StartsWith("a", StringComparison.InvariantCultureIgnoreCase))   //if more than two words, consider starting with the letter "a"
                 {
-                    Console.Write("Введены не корректные параметры, попробуйте снова");
+                    count += 1;
                 }
             }
+            string positivanswer = $"Количество слов, на чинающихся с буквы 'А' равно {count}";
+            string negativeanswer = $"В введенном предложении нет слов, начинающихся с буквы 'А'.";
+            var msg = (count > 0) ? positivanswer : negativeanswer;                 //choose the answer depending on the result of the count
+            Console.WriteLine(msg);
+            return msg;
         }
+
         static void Main(string[] args)
         {
-           Console.WriteLine(CheckStringFormatAndLetterCounter());
+            СalculationString();
         }
     }
 }
