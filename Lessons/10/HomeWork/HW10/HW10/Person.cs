5ce5b0d8f8c005997aaca1f98949c5bedcc63d9a
Juliia Sheikina <sheik1256@bk.ru>
2020-04-19 14:16:53 +0300

    HW10

---
+++ Lessons/10/HomeWork/HW10/HW10/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HW10
+{
+    class Person
+    {
+        private string _name;
+        private int _age;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Value must be not empty");
+                }
+                _name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0 || value > 140)
+                {
+                    throw new ArgumentException("Value must be correct");
+                }
+                _age = value;
+            }
+        }
+
+
+        public int GrowingUp(int years)
+        {
+            if (years < 0)
+            {
+                throw new ArgumentException("Value must be correct");
+            }
+            return Age + years;
+        }
+
+        public string Description =>
+            $"Name: {Name}, age in 4 years: {GrowingUp(years: 4)}";
+    }
+}
---
+++ Lessons/10/HomeWork/HW10/HW10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var persons = new Person[3];
+
+                for (var i = 0; i < persons.Length; i++)
+                {
+                    persons[i] = new Person();
+
+                    Console.Write($"Enter {i + 1} name: ");
+                    persons[i].Name = Console.ReadLine();
+
+                    Console.Write($"Enter {i + 1} age: ");
+                    persons[i].Age = int.Parse(Console.ReadLine());
+                }
+
+                foreach (var person in persons)
+                {
+                    Console.WriteLine(person.Description);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
