ca940c5ea869d9f590a18bb6f1335a34cf695998
Juliia Sheikina <sheik1256@bk.ru>
2020-04-19 14:20:19 +0300

    HW11

---
+++ Lessons/11/HomeWork/HW11/HW11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarm1 = new ReminderItem
+            {
+              AlarmDate = new DateTime(2020,04,19,7,0,0),
+              AlarmMessage = $"Wake up!"
+            };
+            var alarm2 = new ReminderItem
+            {
+                AlarmDate = new DateTime(2020, 04, 17, 15, 30, 0),
+                AlarmMessage = $"Wake up!"
+            };
+            alarm1.WriteProperties();
+            alarm2.WriteProperties();
+        }
+    }
+}
---
+++ Lessons/11/HomeWork/HW11/HW11/ReminderItem.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+
+namespace HW11
+{
+    class ReminderItem
+    {
+        public ReminderItem() { }
+
+        public DateTime AlarmDate { get; set; }
+
+        public string AlarmMessage { get; set; }
+
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return AlarmDate-DateTimeOffset.Now;
+            }
+        }
+
+        public bool IsOutdated
+        {
+            get
+            {
+                return TimeToAlarm >= TimeSpan.Zero;
+            }
+        }
+        public void WriteProperties()
+        {
+            Console.WriteLine(
+                @$"Alarm date: {AlarmDate}
+Alarm message: {AlarmMessage} 
+Time to alarm: {TimeToAlarm} 
+Is out dated: {IsOutdated}");
+        }
+
+    }
+}
