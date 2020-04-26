75175f9409a701a77817d68408cf77a22114d2d0
Juliia Sheikina <sheik1256@bk.ru>
2020-04-26 18:42:46 +0300

    HW13

--- Lessons/13/HomeWork/HW13/HW13/ILogWriter.cs
+++ Lessons/13/HomeWork/HW13/HW13/ILogWriter.cs
@@ -6,6 +6,8 @@ namespace HW13
 {
     interface ILogWriter
     {
+        public DateTimeOffset LogTime { get; set; }
+        public string Message { get; set; }
         void LogInfo(string message) { }
 
         void LogWarning(string message) { }
