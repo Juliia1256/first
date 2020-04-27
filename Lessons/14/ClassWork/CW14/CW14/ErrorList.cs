2d73853973b295d956e1dc2d9829fc8aedaee919
Juliia Sheikina <sheik1256@bk.ru>
2020-04-27 20:49:14 +0300

    CW14

---
+++ Lessons/14/ClassWork/CW14/CW14/ErrorList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW14
+{
+    class ErrorList : IDisposable, IEnumerable<string>
+    {
+        public string Category { get; }
+        public List<string> Errors { get; private set; }
+
+        public ErrorList(string category)
+        {
+            Category = category;
+        }
+
+        public void Add(string errorMessage)
+        {
+            Errors = new List<string> { errorMessage };
+        }
+        public virtual void Dispose()
+        {
+            Errors.Clear();
+
+            Errors = new List<string>();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var iitem = Errors.GetEnumerator();
+            return iitem;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Lessons/14/ClassWork/CW14/CW14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var examp = new ErrorList(category: "error"))
+            {
+                examp.Add("new error");
+                foreach (var item in examp.Errors)
+                {
+                    Console.WriteLine(item);
+                }
+             }
+
+        }
+    }
+}
