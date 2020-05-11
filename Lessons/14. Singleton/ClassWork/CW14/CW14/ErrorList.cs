f906aefd4d8c11e3d0e8cf5833c39a73c50beed0
Juliia Sheikina <sheik1256@bk.ru>
2020-05-11 13:44:53 +0300

    HW14, corrected

---
+++ Lessons/14. Singleton/ClassWork/CW14/CW14/ErrorList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW14
+{
+    static class ErrorList 
+    {
+        public static string Category { get; set; }
+        private static List<string> _errors;
+        public static string OutputPrefixFormat { get; set; }
+
+        private static string _outputPrefix
+        {
+            get
+            {
+                return DateTime.Now.ToString(OutputPrefixFormat);
+            }
+        }
+
+        static ErrorList()
+        {
+            Category = "error";
+            _errors = new List<string>();
+            OutputPrefixFormat = "MMMM d, yyyy (hh:mm tt)";
+        }
+
+        public static void Add(string errorMessage)
+        {
+            _errors.Add(errorMessage);
+        }
+
+
+        public static void WrightToConsole()
+        {
+            foreach(var item in _errors)
+            {
+                Console.WriteLine(string.Format($"{_outputPrefix} {Category} {item}"));
+            }
+            
+        }
+
+
+        public static IEnumerator<string> GetEnumerator()
+        {
+            var iitem = _errors.GetEnumerator();
+            return iitem;
+        }
+    }
+}
---
+++ Lessons/14. Singleton/ClassWork/CW14/CW14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ErrorList.Category = "Errors";
+            ErrorList.Add("new error");
+            ErrorList.Add("some error");
+            ErrorList.WrightToConsole();
+
+
+
+        }
+    }
+}
