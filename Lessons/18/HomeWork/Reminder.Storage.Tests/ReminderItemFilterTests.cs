b0341ef952566cb4f322aa4b3f54199a28d5b66d
Juliia Sheikina <sheik1256@bk.ru>
2020-06-22 11:10:50 +0300

    HW19, corrected

--- Lessons/18/HomeWork/Reminder.Storage.Tests/ReminderItemFilterTests.cs
+++ Lessons/18/HomeWork/Reminder.Storage.Tests/ReminderItemFilterTests.cs
@@ -1,53 +1,40 @@
 using NUnit.Framework;
 using System;
-using Reminder.Storage.Memory;
+
 
 namespace Reminder.Storage.Tests
 {
     public class ReminderItemFilterTests
     {
-        ReminderItemStorage storage;
 
         [SetUp]
         public void Set()
         {
-            storage = new ReminderItemStorage();
-            storage.Add(
-                new ReminderItem(Guid.NewGuid(), "header", "message one", DateTimeOffset.Now.AddMinutes(-1), "User1", ReminderItemStatus.Ready));
-            storage.Add(
-                new ReminderItem(Guid.NewGuid(), "header", "message two", DateTimeOffset.Now.AddMinutes(-1), "User2"));
-            storage.Add(
-                new ReminderItem(Guid.NewGuid(), "header", "message three", DateTimeOffset.Now.AddMinutes(20), "User3"));
-
         }
         [Test]
         public void WhenReminderFilterByStatusReady()
         {
-            var result = storage.FindBy(ReminderItemFilter.ByStatus(ReminderItemStatus.Ready));
-            Assert.IsNotEmpty(result);
-            Assert.AreEqual("message one", result[0].Message);
+            var filter = ReminderItemFilter.ByStatus(ReminderItemStatus.Ready);
+            Assert.NotNull(filter);
+            Assert.IsTrue(filter.IsByStatus);
         }
 
         [Test]
         public void WhenReminderFilterFromNow()
         {
-
-            var result = storage.FindBy(ReminderItemFilter.FromNow());
-            Assert.IsNotEmpty(result);
-            Assert.AreEqual("message two", result[0].Message);
-        }
-        public void WhenReminderFilterByDateTime()
-        {
-
-            var f4 = ReminderItemFilter.ByDateTime(DateTimeOffset.UtcNow);
+            var filter = ReminderItemFilter.FromNow();
+            Assert.NotNull(filter);
+            Assert.IsTrue(filter.IsByStatus);
+            Assert.IsTrue(filter.IsByDateTime);
         }
         [Test]
-        public void WhenReminderFilterByStatusSent_ThenNotFound()
+        public void WhenReminderFilterByDateTime()
         {
-            var result = storage.FindBy(ReminderItemFilter.ByStatus(ReminderItemStatus.Sent));
-            Assert.IsEmpty(result);
-
+            var filter = ReminderItemFilter.ByDateTime(DateTimeOffset.UtcNow);
+            Assert.NotNull(filter);
+            Assert.IsTrue(filter.IsByDateTime);
         }
+
 
     }
 }
