a753b265c5e7a3403381f6641de9c365f71b9b79
Juliia Sheikina <sheik1256@bk.ru>
2020-07-19 20:01:56 +0300

    HW28, corrected

--- Lessons/28/HomeWork/SP_FindByFilter.sql
+++ Lessons/28/HomeWork/SP_FindByFilter.sql
@@ -9,32 +9,20 @@ CREATE PROCEDURE [u_sheykina_schema].[sp_FindByFilter](
 AS
 BEGIN
 set NOCOUNT on;
-if @DateTimeUtc is not null and @ItemStatusId is not null
 BEGIN
-select Id from ReminderItem
-Where DateTimeUtc >= @DateTimeUtc and ItemStatusId = @ItemStatusId;
-RETURN 
-END
-if @ItemStatusId is null
-BEGIN
-select Id from ReminderItem
-Where DateTimeUtc >= @DateTimeUtc
-END
-if @DateTimeUtc is NULL
-BEGIN
-select Id from ReminderItem
-Where ItemStatusId = @ItemStatusId;
+select Id from ReminderItem as RI
+Where (@ItemStatusId is not null and RI.ItemStatusId = @ItemStatusId) OR 
+      (@DateTimeUtc is not null and RI.DateTimeUtc >= @DateTimeUtc)
 RETURN 
 END
 END
 GO
 
-DECLARE @DateTimeUtc datetimeoffset = '2020-07-01 12:00:00.0000000 +01:00';
-DECLARE @ItemStatusId tinyint = 1;
-EXECUTE [u_sheykina_schema].[sp_FindByFilter] @DateTimeUtc, @ItemStatusId;
+DECLARE @DateTimeUtc datetimeoffset = '2020-07-19 12:00:00.0000000 +01:00';
+EXECUTE [u_sheykina_schema].[sp_FindByFilter] @DateTimeUtc;
 
 DECLARE @DateTimeUtc datetimeoffset = '2020-07-01 12:00:00.0000000 +01:00';
-DECLARE @ItemStatusId tinyint = null;
+DECLARE @ItemStatusId TINYINT = 1;
 EXECUTE [u_sheykina_schema].[sp_FindByFilter] @DateTimeUtc, @ItemStatusId;
 
 DECLARE @DateTimeUtc datetimeoffset = null;
